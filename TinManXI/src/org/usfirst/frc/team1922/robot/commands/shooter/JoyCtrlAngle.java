3aae942803234450452e2bc97b5eebedfcb1100d
Kevin Mackenzie <dev.kjmack305@gmail.com>
2016-03-12 11:22:10 -0500

    Added Threshold for Operator Angle Control
    Fixed issue #28  untested

--- TinManXI/src/org/usfirst/frc/team1922/robot/commands/shooter/JoyCtrlAngle.java
+++ TinManXI/src/org/usfirst/frc/team1922/robot/commands/shooter/JoyCtrlAngle.java
@@ -21,7 +21,12 @@ public class JoyCtrlAngle extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.mShooter.GetShooterAngle().SetSpeed(Robot.oi.GetJoystick("OpStick").getY());
+    	//have a hardcoded threshold for the angle
+    	double stickVal = Robot.oi.GetJoystick("OpStick").getY();
+    	if(Math.abs(stickVal) > .15)
+    	{
+    		Robot.mShooter.GetShooterAngle().SetSpeed(stickVal);
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
