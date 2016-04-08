cdefac44183aa810ea159cfc5a35274435350d46
Kevin Mackenzie <dev.kjmack305@gmail.com>
2016-04-08 13:22:38 -0400

    Fixed High Goal Prep

--- TinManXI/src/org/usfirst/frc/team1922/robot/commands/PrepHighGoal.java
+++ TinManXI/src/org/usfirst/frc/team1922/robot/commands/PrepHighGoal.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team1922.robot.commands;
 
 import org.usfirst.frc.team1922.robot.Robot;
 import org.usfirst.frc.team1922.robot.commands.shooter.SetShooterAngle;
+import org.usfirst.frc.team1922.robot.commands.shooter.SpinDownShooterWheels;
+import org.usfirst.frc.team1922.robot.commands.shooter.SpinUpShooterWheels;
 import org.usfirst.frc.team1922.robot.commands.shooter.SpinUpShooterWheelsCfg;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -13,11 +15,11 @@ public class PrepHighGoal extends CommandGroup {
     
     public  PrepHighGoal() {
     	addParallel(new DisableIntakeWheels());
-    	addParallel(new SetShooterAngle(Robot.mShooter.GetShooterAngle().GetHighGoalAngle()));
     	addParallel(new SetIntakePositionMedLow());
-    	addSequential(new EnableIntakeWheels(true));
-    	addSequential(new WaitTime(.05));
-    	addSequential(new DisableIntakeWheels());
+    	addSequential(new SetShooterAngle(Robot.mShooter.GetShooterAngle().GetHighGoalAngle()));
+    	addSequential(new SpinUpShooterWheels(-500));
+    	addSequential(new WaitTime(.125));
+    	addSequential(new SpinDownShooterWheels());
     	addSequential(new SpinUpShooterWheelsCfg());
     }
 }
--- TinManXI/src/org/usfirst/frc/team1922/robot/commands/shooter/JoyCtrlAngle.java
+++ TinManXI/src/org/usfirst/frc/team1922/robot/commands/shooter/JoyCtrlAngle.java
@@ -23,10 +23,14 @@ public class JoyCtrlAngle extends Command {
     protected void execute() {
     	//have a hardcoded threshold for the angle
     	double stickVal = Robot.oi.GetJoystick("OpStick").getY();
-    	//if(Math.abs(stickVal) > .15)
-    	//{
+    	if(Math.abs(stickVal) > 0.15)
+    	{
     		Robot.mShooter.GetShooterAngle().SetSpeed(stickVal);
-    	//}
+    	}
+    	else
+    	{
+    		Robot.mShooter.GetShooterAngle().SetSpeed(0);
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
